ab3f1dcf0692e3143af466926208969f2f91a01e
jommelsuril <62045984+jommelsuril@users.noreply.github.com>
2020-05-04 13:33:30 +0800

    Update index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+      $servername = "420server.mysql.database.azure.com";
+      $username = "JomSur@420server";
+      $password = "MrDop33098";
+      $dbname = "it330db";
+
+      // Create connection
+      $conn = mysqli_connect($servername, $username, $password, $dbname);
+      // Check connection
+      if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+      }
+
+      $sql = "SELECT id, description, count FROM Inventory";
+      $result = mysqli_query($conn, $sql);
+
+      if (mysqli_num_rows($result) > 0) {
+          // output data of each row
+          while($row = mysqli_fetch_assoc($result)) {
+              echo "id: " . $row["id"]. " - Product: " . $row["description"]. " - Count" . $row["count"]. "<br>";
+          }
+      } else {
+          echo "0 results";
+      }
+
+      mysqli_close($conn);
+?>
